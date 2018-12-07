ab4dc175b84c0611fa72c042d812b4ed68ed4581
Jean-Robin Foehn <jrfoehn@gmail.com>
2018-12-07 18:04:37 +0100

    v1

---
+++ frontend/src/components/ParticipantInfo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-layout row wrap>
+            <v-flex xs6>
+                <v-text-field prepend-icon="person" name="name" label="Name" type="text"></v-text-field>
+            </v-flex>
+            <v-spacer></v-spacer> 
+            <v-flex xs6>
+                <v-text-field prepend-icon="email" name="email" label="Email" type="email"></v-text-field>
+            </v-flex>             
+        </v-layout>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ParticipantInfo",
+    props: {
+    }
+};
+</script>
--- frontend/src/components/SecretSanta.vue
+++ frontend/src/components/SecretSanta.vue
@@ -9,14 +9,22 @@
             </v-toolbar>
             <v-card-text>
               <v-form>
-                <v-text-field prepend-icon="person" name="name" label="Name" type="text"></v-text-field>
-                <v-text-field prepend-icon="alternate_email" name="email" label="Email" type="email"></v-text-field>
+                <!-- <ParticipantInfo></ParticipantInfo> -->
+                <v-layout row wrap v-for="(participant, key) in participants">
+                    <v-flex xs6>
+                        <v-text-field prepend-icon="person" name="name" label="Name" type="text" v-model="participants[key].name"></v-text-field>
+                    </v-flex>
+                    <v-spacer></v-spacer> 
+                    <v-flex xs6>
+                        <v-text-field prepend-icon="email" name="email" label="Email" type="email" v-model="participants[key].email"></v-text-field>
+                    </v-flex>             
+                </v-layout>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary">Add Participant</v-btn>
-              <v-btn color="red">Send</v-btn>
+              <v-btn color="primary" @click="addParticipant(key)">Add</v-btn>
+              <v-btn color="red" @click="rudolf()">Send</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -26,10 +34,39 @@
 </template>
 
 <script>
+import axios from "axios";
+import ParticipantInfo from "./ParticipantInfo";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  components: {
+    ParticipantInfo
+  },
+  data(){
+    return {
+      participants: []
+    }
+  },
+  methods: {
+    addParticipant() {
+      this.participants.push({ name: '', email: ''});
+    },
+    rudolf() {
+      let data = []
+      this.participants.forEach(user => {
+        console.log(user.name, user.email)
+        data.push([user.name, user.email])
+      }); 
+      axios.post("https://8goak50z6d.execute-api.eu-west-1.amazonaws.com/Lab-ServerlessSecretSanta/santa", {
+        data: data
+      })
+      .then(response => {})
+      .catch(e => {
+        this.errors.push(e)
+      });
+    }
+  },
+  created(){
+    this.addParticipant();
   }
 };
 </script>
